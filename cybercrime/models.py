349efb983acb0760976d069e9dd2e6adbd790638
LikithaRamamoorthi <Likitha9788@gmail.com>
2026-03-23 14:55:19 +0530

    updated models and views in cybercrime

--- cybercrime/models.py
+++ cybercrime/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class PhishingData(models.Model):
+    url_length          = models.IntegerField()
+    has_ip              = models.BooleanField(default=False)
+    suspicious_keywords = models.IntegerField(default=0)
+    has_https           = models.BooleanField(default=True)
+    result              = models.CharField(max_length=20)
+    submitted_at        = models.DateTimeField(auto_now_add=True)
+
+class CyberModel(models.Model):
+    algorithm  = models.CharField(max_length=100)
+    accuracy   = models.FloatField()
+    trained_on = models.DateTimeField(auto_now_add=True)
+
+
 # Create your models here.
