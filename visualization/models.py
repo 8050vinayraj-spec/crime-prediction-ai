71a5a05a15307b27ac319ce761cfba60f4068e23
Vinay Raj <8050vinayraj@gmail.com>
2026-03-23 15:17:36 +0530

    updates in vizualtlition

--- visualization/models.py
+++ visualization/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class PredictionLog(models.Model):
+    MODULE_CHOICES = [('crime', 'Crime'), ('cyber', 'Cybercrime')]
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    module     = models.CharField(max_length=10, choices=MODULE_CHOICES)
+    input_data = models.JSONField()
+    result     = models.CharField(max_length=200)
+    timestamp  = models.DateTimeField(auto_now_add=True)
+
